31a1ef6f108068a207e5a841371986935f02a35b
Geralt of Rivia <alex@AlexH.local>
2020-07-08 20:59:54 -0500

    add header and footer | update head to include styles

---
+++ app/public/wp-content/themes/learning-university-theme/footer.php
@@ -0,0 +1,6 @@
+
+
+    <h4>The footer</h4>
+    <?php wp_footer(); ?>
+  </body>
+</html>
--- app/public/wp-content/themes/learning-university-theme/page.php
+++ app/public/wp-content/themes/learning-university-theme/page.php
@@ -1,10 +1,14 @@
 <?php
 
+  get_header();
+
   while(have_posts()) {
     the_post(); ?>
     <h1>This is a Page, not a post.</h1>
     <h3><?php the_title(); ?></h3>
     <?php the_content(); ?>
   <?php }
+
+  get_footer();
 
 ?>
